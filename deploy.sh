cf474b43b96051240f7a485f432bac581764a3ad
kt <kthobbs@mail.ubc.ca>
2024-01-12 10:50:10 -0800

    rebuilding site Fri 12 Jan 2024 10:50:10 PST

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 # after running 'hugo server' to render updates,
 # copy contents to the docs/ folder for gh-pages to read
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+hugo -t hugo-coder
+
 cp -r public/ docs/
 
 # commit
